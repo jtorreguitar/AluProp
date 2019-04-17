b7b50dafeae57e97fe3375c5ca4debfc7b5d148d
Clara Guzzetti <cguzzetti@itba.edu.ar>
2019-04-17 15:34:07 -0300

    Add initial style (#3)
    * add initial style
    
    * Create resource handler and connect css file
    
    * Update css style
    
    * Allign content and navbar
    
    * Transform elements to cards
    
    * Add some style changes & create more dummy cards to see real layout
    
    * Move search-bar style to a more specific class
    
    * Add extra space at the end of file

--- webapp/src/main/webapp/ar/edu/itba/paw/webapp/config/WebConfig.java
+++ webapp/src/main/webapp/ar/edu/itba/paw/webapp/config/WebConfig.java
@@ -13,13 +13,15 @@ import org.springframework.jdbc.datasource.init.DatabasePopulator;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @EnableWebMvc
 @ComponentScan({ "ar.edu.itba.paw.webapp.controller", "ar.edu.itba.paw.persistence", "ar.edu.itba.paw.service" })
 @Configuration
-public class WebConfig {
+public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Value("classpath:schema.sql")
     private Resource schemaSql;
@@ -37,8 +39,8 @@ public class WebConfig {
         final SimpleDriverDataSource ds = new SimpleDriverDataSource();
         ds.setDriverClass(org.postgresql.Driver.class);
         ds.setUrl("jdbc:postgresql://localhost/paw");
-        ds.setUsername("jtorreguitar");
-        ds.setPassword("12345aA_");
+        ds.setUsername("postgres");
+        ds.setPassword("postgres");
         return ds;
     }
 
@@ -54,5 +56,10 @@ public class WebConfig {
         final ResourceDatabasePopulator dbp = new ResourceDatabasePopulator();
         dbp.addScript(schemaSql);
         return dbp;
+    }
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry){
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
     }
 }
