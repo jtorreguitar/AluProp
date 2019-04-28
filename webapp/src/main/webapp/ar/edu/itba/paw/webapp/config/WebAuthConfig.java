1c3fb39b8b2688022dcb07dc177c59886d338d46
jtorreguitar <jtorreguitar@itba.edu.ar>
2019-04-28 16:03:47 -0300

    removed unused imports

--- webapp/src/main/webapp/ar/edu/itba/paw/webapp/config/WebAuthConfig.java
+++ webapp/src/main/webapp/ar/edu/itba/paw/webapp/config/WebAuthConfig.java
@@ -1,6 +1,5 @@
 package ar.edu.itba.paw.webapp.config;
 
-import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import ar.edu.itba.paw.webapp.auth.APUserDetailsService;
@@ -15,14 +14,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @Configuration
 @EnableWebSecurity
