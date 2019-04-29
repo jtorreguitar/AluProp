50331db4045e65ee838a864ca09d7c2d4ba0c65c
jtorreguitar <jtorreguitar@itba.edu.ar>
2019-04-28 22:07:22 -0300

    rebase from master

--- interfaces/src/main/java/ar/edu/itba/paw/interfaces/PropertyDao.java
+++ interfaces/src/main/java/ar/edu/itba/paw/interfaces/PropertyDao.java
@@ -1,8 +1,9 @@
 package ar.edu.itba.paw.interfaces;
 
+import ar.edu.itba.paw.interfaces.dao.Dao;
 import ar.edu.itba.paw.model.Property;
 
-public interface PropertyDao extends Dao<Property>{
+public interface PropertyDao extends Dao<Property> {
 
 	boolean showInterest(int propertyId, String email, String description);
     Property getPropertyWithRelatedEntities(long id);
--- interfaces/src/main/java/ar/edu/itba/paw/interfaces/dao/PropertyDao.java
+++ interfaces/src/main/java/ar/edu/itba/paw/interfaces/dao/PropertyDao.java
@@ -4,6 +4,7 @@ import ar.edu.itba.paw.model.Property;
 import ar.edu.itba.paw.model.User;
 
 public interface PropertyDao extends Dao<Property>{
-
+	Property get(long id);
 	boolean showInterest(int propertyId, User user);
+	Property getPropertyWithRelatedEntities(long id);
 }
