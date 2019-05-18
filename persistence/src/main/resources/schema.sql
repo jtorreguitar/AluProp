9eafb2bef3f4f516e6e9b6b98c190fb7ca8f20ca
Bensas <juanbensa@gmail.com>
2019-05-18 16:48:34 -0300

    Deploy now works. Remember to create the appropriate credentials in local database!

--- persistence/src/main/resources/schema.sql
+++ persistence/src/main/resources/schema.sql
@@ -22,17 +22,6 @@ CREATE TABLE IF NOT EXISTS neighbourhoods (
     cityId INTEGER REFERENCES cities(id) ON DELETE SET NULL
 );
 
-CREATE TABLE IF NOT EXISTS properties (
-    id SERIAL PRIMARY KEY,
-    description varchar(3000),
-    caption varchar(250),
-    propertyType varchar(100),
-    neighbourhoodId integer references neighbourhoods(id) ON DELETE SET NULL,
-    privacyLevel boolean,
-    capacity integer,
-    price float
-);
-
 CREATE TABLE IF NOT EXISTS images (
     id SERIAL PRIMARY KEY,
     propertyId INTEGER REFERENCES properties(id) ON DELETE CASCADE,
@@ -40,7 +29,7 @@ CREATE TABLE IF NOT EXISTS images (
 );
 
 -- The statement "ADD COLUMN IF NOT EXISTS" was added to postgres in version 9.6, watch out for version to use in prod
-ALTER TABLE properties ADD COLUMN IF NOT EXISTS mainImageId integer references images(id) ON DELETE SET NULL;
+-- ALTER TABLE properties ADD COLUMN IF NOT EXISTS mainImageId integer references images(id) ON DELETE SET NULL;
 
 CREATE TABLE IF NOT EXISTS universities (
     id SERIAL PRIMARY KEY,
@@ -65,6 +54,19 @@ CREATE TABLE IF NOT EXISTS users (
     universityId INTEGER REFERENCES universities(id) ON DELETE SET NULL,
     careerId INTEGER REFERENCES careers(id) ON DELETE SET NULL,
     role varchar(50)
+);
+
+CREATE TABLE IF NOT EXISTS properties (
+    id SERIAL PRIMARY KEY,
+    description varchar(3000),
+    caption varchar(250),
+    propertyType varchar(100),
+    neighbourhoodId integer references neighbourhoods(id) ON DELETE SET NULL,
+    privacyLevel boolean,
+    capacity integer,
+    price float,
+    mainImageId integer references images(id) ON DELETE SET NULL,
+    ownerId INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS interests (
@@ -140,6 +142,3 @@ ON CONFLICT DO NOTHING;
 INSERT INTO careers(id, name)
 VALUES (1, 'Ingeniería informática')
 ON CONFLICT DO NOTHING;
-
-ALTER TABLE properties
-ADD COLUMN IF NOT EXISTS ownerId INTEGER REFERENCES users(id) ON DELETE CASCADE;
