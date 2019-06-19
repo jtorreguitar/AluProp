74d66ae0f37ba6cc30a203f9a8f0102760b2a6c4
Ignacio Vidaurreta <igvidaurreta@gmail.com>
2019-06-18 23:01:00 -0300

    Create more tests for userDao

---
+++ persistence/src/test/java/ar/edu/itba/paw/persistence/Factories.java
@@ -0,0 +1,25 @@
+package ar.edu.itba.paw.persistence;
+
+import ar.edu.itba.paw.model.User;
+import ar.edu.itba.paw.model.enums.Gender;
+import ar.edu.itba.paw.model.enums.Role;
+
+import java.sql.Date;
+
+public class Factories {
+    public static User userCreator(){
+        User.Builder builder = new User.Builder();
+        return builder.withEmail(APUserDaoTest.EMAIL2)
+                .withName(APUserDaoTest.NAME2)
+                .withLastName("Tester")
+                .withBirthDate(Date.valueOf("1996-07-12"))
+                .withGender(Gender.MALE)
+                .withPasswordHash("SuperS3CReTP4ssW0rd")
+                .withUniversityId(1)
+                .withCareerId(1)
+                .withBio("Hello, how are you today?")
+                .withContactNumber("1166765456")
+                .withRole(Role.ROLE_GUEST)
+                .build();
+    }
+}
